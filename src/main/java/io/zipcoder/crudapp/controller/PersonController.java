67d264215242e3a3104a0756b51113dbb7e08706
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-03-29 12:04:15 -0400

    Created Person entity, repository, and controller

---
+++ src/main/java/io/zipcoder/crudapp/controller/PersonController.java
@@ -0,0 +1,51 @@
+package io.zipcoder.crudapp.controller;
+
+
+import io.zipcoder.crudapp.entity.Person;
+import io.zipcoder.crudapp.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class PersonController {
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping("/people")
+    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+        return new ResponseEntity<>(personRepository.save(person), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/people/{id}")
+    public ResponseEntity<Person> getPerson(@PathVariable int id) {
+        return new ResponseEntity<>(personRepository.findOne(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/people")
+    public ResponseEntity<List<Person>> getPersonList() {
+        return new ResponseEntity<>((List<Person>) personRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PutMapping("/people/{id}")
+    public ResponseEntity<Person> updatePerson(@PathVariable int id, @RequestBody Person person) {
+        if (personRepository.findOne(id) != null) {
+            personRepository.delete(person.getId());
+            return new ResponseEntity<>(personRepository.save(person), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(personRepository.save(person), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/people/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deletePerson(@PathVariable int id) {
+        personRepository.delete(id);
+    }
+}
---
+++ src/main/java/io/zipcoder/crudapp/entity/Person.java
@@ -0,0 +1,46 @@
+package io.zipcoder.crudapp.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
